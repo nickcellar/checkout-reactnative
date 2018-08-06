eb4d89e98ad49b66796c720e2334b1fd93a140a1
Nicholas Wong <nickwph@gmail.com>
2018-08-06 22:44:21 +0800

    Able to add items into checkout.

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -1,6 +1,7 @@
-import {Button} from "react-native";
+import {Button, View} from "react-native";
 import React from "react";
 import {items} from "../models/ItemData";
+import {Card, ListItem} from "react-native-elements";
 
 export class CheckoutScreen extends React.Component {
 
@@ -8,23 +9,50 @@ export class CheckoutScreen extends React.Component {
     title: 'Checkout',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedItems: []
+    }
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <Button
-        title="New Checkout"
-        onPress={() =>
-          navigate('NewItem', {
-            onItemSelected: this.onItemSelected.bind(this)
-          })
-        }
-      />
+      <View>
+        <Card containerStyle={{padding: 0}}>
+          {
+            this.state.selectedItems.map((u, i) => {
+              return (
+                <ListItem
+                  key={i}
+                  roundAvatar
+                  title={u.name}
+                  avatar={{uri: u.avatar}}
+                />
+              );
+            })
+          }
+        </Card>
+        <Button
+          title="New Checkout"
+          onPress={() =>
+            navigate('NewItem', {
+              onItemSelected: this.onItemSelected.bind(this)
+            })
+          }
+        />
+      </View>
     );
   }
 
   onItemSelected(selectedItemKey) {
-    let item = items.find(item => item.key === selectedItemKey);
     console.log(`Adding item with key ${selectedItemKey} into checkout list`);
+    let item = items.find(item => item.key === selectedItemKey);
     console.log(item);
+    // this.selectedItems.push(item);
+    this.setState(prevState => ({
+      selectedItems: [...prevState.selectedItems, item]
+    }))
   }
 }
