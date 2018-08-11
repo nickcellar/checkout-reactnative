c716b34688e5c7aa6e23930ad367a72de7df198f
Nicholas Wong <nickwph@gmail.com>
2018-08-12 03:32:18 +0800

    Able to dynamically enable checkout icon.

--- src/components/cart/CartPage.js
+++ src/components/cart/CartPage.js
@@ -4,14 +4,14 @@ import {clearCartAction} from "../../actions/cartActions";
 import {addRecordAction} from "../../actions/recordActions";
 import {CheckoutAlert} from "./CheckoutAlert";
 import {CartContainer} from "./Cart";
-import {CheckoutIcon} from "./CheckoutIcon";
+import {CheckoutIconContainer} from "./CheckoutIcon";
 
 export class CartPage extends React.Component {
 
   static navigationOptions = ({navigation}) => {
     return ({
       title: 'Your shopping cart',
-      headerRight: (<CheckoutIcon onPress={() => navigation.state.params.checkout()}/>),
+      headerRight: (<CheckoutIconContainer onPress={() => navigation.state.params.checkout()}/>),
     })
   };
 
--- src/components/cart/CheckoutIcon.js
+++ src/components/cart/CheckoutIcon.js
@@ -1,5 +1,9 @@
 import React from "react";
 import {Icon} from "react-native-elements";
+import {CartPage} from "./CartPage";
+import {clearCartAction} from "../../actions/cartActions";
+import {addRecordAction} from "../../actions/recordActions";
+import {connect} from "react-redux";
 
 
 export class CheckoutIcon extends React.Component {
@@ -8,11 +12,33 @@ export class CheckoutIcon extends React.Component {
     return (
       <Icon
         containerStyle={{marginRight: 16}}
-        onPress={() => this.props.onPress()}
+        onPress={() => this.isReady() && this.props.onPress()}
         name='md-checkmark'
-        color='#000000'
+        color={this.isReady() ? '#000000' : '#bbbbbb'}
         type='ionicon'
       />
     )
   }
+
+  isReady() {
+    return this.props.cart.productKeys.length > 0
+  }
 }
+
+const mapStateToProps = state => {
+  return ({
+    cart: state.cart
+  })
+};
+
+const mapDispatchToProps = dispatch => ({
+  checkout: (cart) => {
+    dispatch(addRecordAction(cart));
+    dispatch(clearCartAction());
+  }
+});
+
+export const CheckoutIconContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CheckoutIcon);
