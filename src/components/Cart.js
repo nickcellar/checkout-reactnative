34551fa7177a0f26a6b69d72446056abe20588fc
Nicholas Wong <nickwph@gmail.com>
2018-08-12 01:45:21 +0800

    Updated cart component.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import {View} from "react-native";
 import React from "react";
-import {Card} from "react-native-elements";
+import {Card, Text} from "react-native-elements";
 import {connect} from "react-redux";
 import {ProductListItem} from "./ProductListItem";
 import {DiscountListItem} from "./DiscountListItem";
@@ -27,37 +27,32 @@ export class Cart extends React.Component {
         CheckoutAlert(this.props.cart.totalPrice, () => {
           this.props.checkout(this.props.cart);
           this.props.navigation.goBack();
-
         })
       }
     });
   }
 
   render() {
-    if (this.props.cart.productKeys.length === 0) {
-      return (
-        <AddMoreButton
-          message="Add your first item"
-          onPress={() => this.props.navigation.navigate(PAGE_PRODUCT_PICKER)}/>
-      )
-    }
     return (
       <View>
-        <Card
-          containerStyle={{paddingBottom: 0}}
-          title={`TOTAL PRICE: $${this.props.cart.totalPrice}`}>
-          {this.props.cart.products.map((product, index) => (
-            <ProductListItem key={`product-${index}`} product={product}/>
-          ))}
-          {this.props.cart.discounts.map((discount, index) => (
-            <DiscountListItem key={`discount-${index}`} discount={discount}/>
-          ))}
-          {this.props.cart.freeItems.map((freeItem, index) => (
-            <FreeItemListItem key={`freeItem-${index}`} freeItem={freeItem}/>
-          ))}
-        </Card>
+        <Text style={{padding: 16}}>Customer: {this.props.currentCustomerKey}</Text>
+        {this.props.cart.productKeys.length > 0 && (
+          <Card
+            containerStyle={{paddingBottom: 0}}
+            title={`TOTAL PRICE: $${this.props.cart.totalPrice}`}>
+            {this.props.cart.products.map((product, index) => (
+              <ProductListItem key={`product-${index}`} product={product}/>
+            ))}
+            {this.props.cart.discounts.map((discount, index) => (
+              <DiscountListItem key={`discount-${index}`} discount={discount}/>
+            ))}
+            {this.props.cart.freeItems.map((freeItem, index) => (
+              <FreeItemListItem key={`freeItem-${index}`} freeItem={freeItem}/>
+            ))}
+          </Card>
+        )}
         <AddMoreButton
-          message="Add more"
+          message={this.props.cart.productKeys.length > 0 ? "Add more" : "Add your first item"}
           onPress={() => this.props.navigation.navigate(PAGE_PRODUCT_PICKER)}/>
       </View>
     );
