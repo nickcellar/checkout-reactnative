a72c86b98b717450b8e1688647d0c66c1045b2a5
Nicholas Wong <nickwph@gmail.com>
2018-08-11 16:16:40 +0800

    Added customer reducer.

---
+++ src/reducers/customerReducers.js
@@ -0,0 +1,21 @@
+import {customers} from "../models/customers";
+
+export const customerReducers = (state = customers, action) => {
+
+  // console.debug("Cart reducers");
+  // console.debug("> action", action);
+  // console.debug("> state", state);
+
+  let newState;
+
+  switch (action.type) {
+
+    default:
+      // console.debug("Getting initial cart");
+      newState = state;
+      break;
+  }
+
+  // console.debug("> newState", newState);
+  return newState;
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import {cartReducers} from "./cartReducers";
+import {customerReducers} from "./customerReducers";
 
 export const reducers = combineReducers({
   cart: cartReducers,
+  customers: customerReducers,
 });
