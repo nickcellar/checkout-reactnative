4fbfe1d3ec14b31d324590471e576117133a6032
Nicholas Wong <nickwph@gmail.com>
2018-08-11 21:33:31 +0800

    Cleaned up customer picker.

--- src/components/CustomerPicker.js
+++ src/components/CustomerPicker.js
@@ -21,37 +21,30 @@ export class CustomerPicker extends React.Component {
   }
 
   render() {
-    const {navigate} = this.props.navigation;
     return (
       <View>
+        <RecordListContainer/>
         <View style={{margin: 14}}>
-          <RecordListContainer/>
           <Text style={{marginBottom: 8}}>Please select the client:</Text>
           <Picker
             style={{backgroundColor: "#ffffff", borderColor: "#444444"}}
             mode={"dropdown"}
             selectedValue={this.state.customerKey}
-            onValueChange={(key, index) => {
-              this.setState({customerKey: key})
-            }}>
-            {
-              this.props.customers.map((customer, index) => {
-                return (
-                  <Picker.Item
-                    styles={{background: "#ffffff"}}
-                    label={customer.name}
-                    value={customer.key}
-                    key={customer.key}/>
-                )
-              })
-            }
+            onValueChange={(key, index) => this.setState({customerKey: key})}>
+            {this.props.customers.map((customer, index) => (
+              <Picker.Item
+                styles={{background: "#ffffff"}}
+                label={customer.name}
+                value={customer.key}
+                key={customer.key}/>
+            ))}
           </Picker>
         </View>
         <Button
           title="New checkout"
           onPress={() => {
             this.props.setCurrentCustomer(this.state.customerKey);
-            navigate(PAGE_CART)
+            this.props.navigation.navigate(PAGE_CART)
           }}
         />
       </View>
